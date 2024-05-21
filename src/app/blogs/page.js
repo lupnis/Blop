f4eaad3cc5ab76e3bf329b5643661b3ca8e9d6b8
Lupnis J H <Lupnis@outlook.com>
2024-05-21 18:17:35 +0800

    feat: update blogs page

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,28 +1,19 @@
 "use client";
 
-/** installed modules import  */
 import * as React from "react";
-import { useState, useEffect } from "react"
 
-/** components import */
 import ScrollTop from "@/components/client/components/ScrollTop";
 import BlogListRender from "@/components/client/components/BlogListRender";
+import BlogDocRender from "@/components/client/components/BlogDocRender";
 
-/** configs import */
 import { BlogsPage } from "@/configs/pages/blogs";
 import { Navigate } from "@/configs/navigate";
 
-/** installed styles import */
-//
-
-/** local styles import */
 import "@/assets/css/blogs.css"
 
 export default function BlogPageRender(props) {
     const blogPath = props.searchParams[Navigate.blogs.blogPageParamKey];
     if (!blogPath) {
-        // todo: render list and search page
-        // page title = default
         return (
             <main className="max-w-full">
                 <ScrollTop sticked="drawer-box" />
@@ -31,13 +22,10 @@ export default function BlogPageRender(props) {
             </main>
         );
     }
-    // else todo: render blog article
-
     return (
         <main className="max-w-full">
             <ScrollTop sticked="drawer-box" />
-            <title>%blog title%</title>
-            specific blog
+            <BlogDocRender path={blogPath} />
         </main>
-    ); 
+    );
 }
